f106238db826cae7531be70ffe7ee39e96c9f6d3
kairos4213 <jamesdkairos@gmail.com>
2024-05-27 13:57:13 -0500

    Assignment 3: block to html

--- src/block_md.py
+++ src/block_md.py
@@ -1,6 +1,6 @@
-import re
-
-from htmlnode import HTMLNode
+from htmlnode import ParentNode
+from inline_md import text_to_textnodes
+from textnode import text_node_to_html_node
 
 block_type_paragraph = "paragraph"
 block_type_heading = "heading"
@@ -21,7 +21,7 @@ def markdown_to_blocks(markdown):
     return block_list
 
 
-def block_to_block(markdown_block):
+def block_to_block_type(markdown_block):
     lines = markdown_block.splitlines()
 
     if (
@@ -33,15 +33,14 @@ def block_to_block(markdown_block):
         or markdown_block.startswith("###### ")
     ):
         return block_type_heading
-    elif len(lines) > 1 and lines[0].startswith("```") and lines[-1].endswith("```"):
-        print(lines[0], lines[-1])
+    elif len(lines) >= 1 and lines[0].startswith("```") and lines[-1].endswith("```"):
         return block_type_code
     elif markdown_block.startswith(">"):
         for line in lines:
             if not line.startswith(">"):
                 return block_type_paragraph
         return block_type_quote
-    elif markdown_block.startswith("* ") or markdown_block.startswith("* "):
+    elif markdown_block.startswith("- ") or markdown_block.startswith("* "):
         for line in lines:
             if not line.startswith("- ") and not line.startswith("* "):
                 return block_type_paragraph
@@ -54,3 +53,116 @@ def block_to_block(markdown_block):
         return block_type_ordered_list
     else:
         return block_type_paragraph
+
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+
+
+def quote_block_to_html(markdown_block):
+    lines = markdown_block.split("\n")
+    quote = ""
+    for item in lines:
+        quote += item.lstrip(">") + " "
+    html_node = ParentNode("blockquote", text_to_children(quote.rstrip()))
+    return html_node
+
+
+def ul_block_to_html(markdown_block):
+    list_items = markdown_block.split("\n")
+    html_list = []
+    for item in list_items:
+        item = item.lstrip("- ")
+        item = item.lstrip("* ")
+        html_list.append(ParentNode("li", text_to_children(item)))
+    html_node = ParentNode("ul", html_list)
+    return html_node
+
+
+def ol_block_to_html(markdown_block):
+    list_items = markdown_block.split("\n")
+    html_list = []
+    for i in range(len(list_items)):
+        incrementor = i + 1
+        list_items[i] = list_items[i].lstrip(f"{incrementor}. ")
+        html_list.append(ParentNode("li", text_to_children(list_items[i])))
+    html_node = ParentNode("ol", html_list)
+    return html_node
+
+
+def code_block_to_html(markdown_block):
+    code_text = markdown_block.strip("```")
+    children = text_to_children(code_text)
+    html_node = ParentNode("pre", [ParentNode("code", children)])
+    return html_node
+
+
+def heading_block_to_html(markdown_block):
+    html_node = None
+    if markdown_block.startswith("# "):
+        text = markdown_block.lstrip("# ")
+        children = text_to_children(text)
+        html_node = ParentNode("h1", children)
+    elif markdown_block.startswith("## "):
+        text = markdown_block.lstrip("## ")
+        children = text_to_children(text)
+        html_node = ParentNode("h2", children)
+    elif markdown_block.startswith("### "):
+        text = markdown_block.lstrip("### ")
+        children = text_to_children(text)
+        html_node = ParentNode("h3", children)
+    elif markdown_block.startswith("#### "):
+        text = markdown_block.lstrip("#### ")
+        children = text_to_children(text)
+        html_node = ParentNode("h4", children)
+    elif markdown_block.startswith("##### "):
+        text = markdown_block.lstrip("##### ")
+        children = text_to_children(text)
+        html_node = ParentNode("h5", children)
+    else:
+        text = markdown_block.lstrip("###### ")
+        children = text_to_children(text)
+        html_node = ParentNode("h6", children)
+    return html_node
+
+
+def paragraph_block_to_html(markdown_block):
+    lines = markdown_block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    html_node = ParentNode("p", children)
+    return html_node
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type == block_type_heading:
+            children.append(heading_block_to_html(block))
+        elif block_type == block_type_code:
+            children.append(code_block_to_html(block))
+        elif block_type == block_type_ordered_list:
+            children.append(ol_block_to_html(block))
+        elif block_type == block_type_unordered_list:
+            children.append(ul_block_to_html(block))
+        elif block_type == block_type_quote:
+            children.append(quote_block_to_html(block))
+        else:
+            children.append(paragraph_block_to_html(block))
+
+    html_node = ParentNode("div", children)
+    return html_node
+
+
+md_doc = f"# Heading\n\nThis is **some** text right here\nHere's more text\n\n## Heading 2\n\n### Heading 3\n\n#### Heading 4\n\n##### Heading 5\n\n###### heading 6\n\n```Some code will go here And here```\n\n1. One nice ordered list\n2. With **two** items\n\n* An unordered *italic* list\n- With Different ways to mark it\n- `And a line of code`\n\n>Finished with a nice *quote*\n>From your favorite noobie\n>Me - Newbster McGee".lower()
+
+node = markdown_to_html_node(md_doc)
+print(node.to_html())
--- src/test_block_md.py
+++ src/test_block_md.py
@@ -2,7 +2,8 @@ import unittest
 
 from block_md import (
     markdown_to_blocks,
-    block_to_block,
+    markdown_to_html_node,
+    block_to_block_type,
     block_type_heading,
     block_type_paragraph,
     block_type_quote,
@@ -33,43 +34,50 @@ class TestBlockMd(unittest.TestCase):
 
     def test_block_to_block_heading(self):
         heading = "###### 123"
-        self.assertEqual(block_to_block(heading), block_type_heading)
+        self.assertEqual(block_to_block_type(heading), block_type_heading)
 
     def test_block_to_block_heading_fail(self):
         heading = "########## 1234"
-        self.assertEqual(block_to_block(heading), block_type_paragraph)
+        self.assertEqual(block_to_block_type(heading), block_type_paragraph)
 
     def test_block_to_block_quote(self):
         quote = f">This is a quote\n>More of the quote\n>Quote"
-        self.assertEqual(block_to_block(quote), block_type_quote)
+        self.assertEqual(block_to_block_type(quote), block_type_quote)
 
     def test_block_to_block_quote_fail(self):
         quote = f">This is a quote\nMore of the quote\n>Quote"
-        self.assertEqual(block_to_block(quote), block_type_paragraph)
+        self.assertEqual(block_to_block_type(quote), block_type_paragraph)
 
     def test_block_to_block_code(self):
-        code = f'```This is code.\nHere is more code```'
-        self.assertEqual(block_to_block(code), block_type_code)
+        code = f"```This is code.\nHere is more code```"
+        self.assertEqual(block_to_block_type(code), block_type_code)
 
     def test_block_to_block_code_fail(self):
         code = f"```This is code. Here is more code``"
-        self.assertEqual(block_to_block(code), block_type_paragraph)
+        self.assertEqual(block_to_block_type(code), block_type_paragraph)
 
     def test_block_to_block_unordered_list(self):
         ul_list = f"* This is part of the ul list\n- So is this\n* And this"
-        self.assertEqual(block_to_block(ul_list), block_type_unordered_list)
+        self.assertEqual(block_to_block_type(ul_list), block_type_unordered_list)
 
     def test_block_to_block_unordered_list_fail(self):
         ul_list = f"* This is part of the ul list\n So is this\n* And this"
-        self.assertEqual(block_to_block(ul_list), block_type_paragraph)
+        self.assertEqual(block_to_block_type(ul_list), block_type_paragraph)
 
     def test_block_to_block_ordered_list(self):
         ol_list = f"1. This is an ordered list item\n2. Here's the second item.\n3. And the third"
-        self.assertEqual(block_to_block(ol_list), block_type_ordered_list)
+        self.assertEqual(block_to_block_type(ol_list), block_type_ordered_list)
 
     def test_block_to_block_ordered_list_fail(self):
         ol_list = f"1. This is an ordered list item\n3. Here's the second item.\n3. And the third"
-        self.assertEqual(block_to_block(ol_list), block_type_paragraph)
+        self.assertEqual(block_to_block_type(ol_list), block_type_paragraph)
+
+    def test_markdown_to_html(self):
+        md_doc = f"# Heading\n\nThis is **some** text right here\nHere's more text\n\n## Heading 2\n\n### Heading 3\n\n#### Heading 4\n\n##### Heading 5\n\n###### Heading 6\n\n```Some code will go here And here```\n\n1. One nice ordered list\n2. With **two** items\n\n* An unordered *italic* list\n- With Different ways to mark it\n- `And a line of code`\n\n>Finished with a nice *quote*\n>From your favorite noobie\n>Me - Newbster McGee".lower()
+        html_node = markdown_to_html_node(md_doc)
+        html = f"<div><h1>heading</h1><p>this is <b>some</b> text right here here's more text</p><h2>heading 2</h2><h3>heading 3</h3><h4>heading 4</h4><h5>heading 5</h5><h6>heading 6</h6><pre><code>some code will go here and here</code></pre><ol><li>one nice ordered list</li><li>with <b>two</b> items</li></ol><ul><li>an unordered <i>italic</i> list</li><li>with different ways to mark it</li><li><code>and a line of code</code></li></ul><blockquote>finished with a nice <i>quote</i> from your favorite noobie me - newbster mcgee</blockquote></div>"
+
+        self.assertEqual(html_node.to_html(), html)
 
 
 if __name__ == "__main__":
